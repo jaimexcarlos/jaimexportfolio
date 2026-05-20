249834a9406f0980c1148fb7e5f6cc1504d41544
jaimexcarlos <jaimecarlos.rafer08@gmail.com>
2026-05-20 12:26:48 +0800

    Fix type error in server.ts: cast to any for listen()

--- server.ts
+++ server.ts
@@ -41,7 +41,7 @@ function run(): void {
   const port = process.env['PORT'] || 4000;
 
   // Start up the Node server
-  const server = app();
+  const server: any = app();
   server.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
