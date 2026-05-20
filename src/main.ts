cb4fc7c90cebb1c0b8facfbc77a9695e4237e12b
jaimexcarlos <jaimecarlos.rafer08@gmail.com>
2026-05-20 12:35:13 +0800

    Fix main.ts: remove server imports and process references

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { app } from 'server';
 
 if (environment.production) {
   enableProdMode();
@@ -11,23 +10,14 @@ if (environment.production) {
 
 function bootstrap() {
   platformBrowserDynamic().bootstrapModule(AppModule).then(() => {
-  if ('serviceWorker' in navigator && environment.production) {
-    navigator.serviceWorker.register('ngsw-worker.js');
-  }
-}).catch(err => console.log(err));
-};
+    if ('serviceWorker' in navigator && environment.production) {
+      navigator.serviceWorker.register('ngsw-worker.js');
+    }
+  }).catch(err => console.log(err));
+}
 
-// listen to PORT on localhost
-// if (environment.production) {
-  const port = process.env['PORT'] || 8080;
-  app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`);
-  });
-
-
- if (document.readyState === 'complete') {
-   bootstrap();
- } else {
-   document.addEventListener('DOMContentLoaded', bootstrap);
- }
- 
+if (document.readyState === 'complete') {
+  bootstrap();
+} else {
+  document.addEventListener('DOMContentLoaded', bootstrap);
+}
